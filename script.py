fec13f3c0b471425c989a1200eb70a3f4c9a38c5
Emily Ong <emilyohq11@gmail.com>
2019-03-08 10:58:31 +0800

    edited readme markdown

--- script.py
+++ script.py
@@ -5,13 +5,14 @@ file=open("README.md","w+")
 counter=0
 for i in dirs:
     if not os.path.isfile(i) and i!=".git":
+        file.write("<h2>"+i+"</h2>")
         for j in os.listdir(i):
             if j!=".DS_Store":
                 if os.path.isfile(path+"/"+i+"/"+j):
-                    file.write(i+"/"+j+"\n")
+                    file.write("<b>"+i+"</b>"+"/"+j+"</br>")
                 else:
                     for k in os.listdir(path+"/"+i+"/"+j):
-                        file.write(i+"/"+j+"/"+k+"\n")
-        file.write("\n")
+                        file.write("<b>"+i+"</b>"+"/"+"<b>"+j+"</b>"+"/"+k+"</br>")
+        file.write("</br></br>")
 
 file.close()
